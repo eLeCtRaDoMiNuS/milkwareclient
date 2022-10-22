b46bf2eb175403bf9fecffd4dc9d2c5174613317
llameiq <77410796+eLeCtRaDoMiNuS@users.noreply.github.com>
2022-10-22 20:20:36 +0300

    Create whiteliststuff.lua

---
+++ whiteliststuff.lua
@@ -0,0 +1,24 @@
+_G.key = "MILKWAREKEY4VFJSDF0234-675-GBWFHFGH-F-456-456-HGFMILKINTOPWAREYEAGH-42D-DFG-54FGH-FGER"
+
+function nahhuaintvalid()
+    print("u aint valid sorry :(")
+end
+
+function goodtogo()
+    pcall(loadstring(readfile("milkware/mainscript.lua")))
+end
+
+if _G.key == "MILKWAREKEY4VFJSDF0234-675-GBWFHFGH-F-456-456-HGFMILKINTOPWAREYEAGH-42D-DFG-54FGH-FGER" then
+    goodtogo()
+else
+    nahhuaintvalid()
+end
+
+
+
+game.StarterGui:SetCore("SendNotification",  {
+    Title = "Milkware Client";
+    Text = "key link copied to keyboard go into your browser and paste it there!";
+    Duration = 5;
+    Callback = NotificationBindable;
+})
